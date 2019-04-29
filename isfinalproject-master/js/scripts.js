8a1b052c3bbbc82a3a3bc5cbbd7a3ef6d72402a5
dyuan0226 <33184700+dyuan0226@users.noreply.github.com>
2019-04-29 13:17:25 -0400

    fixed about page

--- isfinalproject-master/js/scripts.js
+++ isfinalproject-master/js/scripts.js
@@ -38,3 +38,14 @@ $(document).ready(function() {
   }
 
 });
+
+function validate(){
+    var zip = $("#zip").val();
+    if(zip != 15213){
+        alert("Please enter a valid zip code.");
+        return false;
+    }
+    var ifrm = parent.document.getElementById("map");
+    ifrm.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d97183.49648518549!2d-80.05054098749426!3d40.43134731658706!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8834f16f48068503%3A0x8df915a15aa21b34!2sPittsburgh%2C+PA!5e0!3m2!1sen!2sus!4v1556500497321!5m2!1sen!2sus";
+    parent.document.getElementById('spots').style.display = 'block';
+}
